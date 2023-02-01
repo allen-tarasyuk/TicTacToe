cef2cd606fdf9895081dad2c0bac4516ff67813b
allen-tarasyuk <allentarasyuk@gmail.com>
2023-01-31 20:18:29 -0800

    A simple welcome screen

---
+++ TestFX/TestFX/src/testfx/Controller.java
@@ -0,0 +1,36 @@
+package testfx;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    @FXML
+    private Label gameTitle;
+
+    @FXML
+    void TwoBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void singleBtn(ActionEvent event) {
+
+
+
+    }
+
+}
